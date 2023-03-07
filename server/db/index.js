7e0f29ae2fc726f8fef202f3a674e23501b877f1
Tulio Torezan <tuliotorezan@gmail.com>
2023-03-07 16:51:04 -0400

    changed DB name from users to guesr

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect('mongodb://127.0.0.1:27017/users', { useNewUrlParser: true })
+    .connect('mongodb://127.0.0.1:27017/guesr', { useNewUrlParser: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
