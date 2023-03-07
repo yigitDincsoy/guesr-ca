33a94942b814d16a32148b36e575146c2d1624d4
Tulio Torezan <tuliotorezan@gmail.com>
2023-03-07 16:32:57 -0400

    added the router to our server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
-const db = require('./data/db')
+const db = require('./db')
+const userRouter = require('./routes/user-router')
 
 const app = express()
 const apiPort = 3000
@@ -16,5 +17,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.use('/api', userRouter)
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
