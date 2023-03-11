20fd48b09b499011fb0c05389a5f3c6ed047899b
Yigit Dincsoy <w0471994@campus.nscc.ca>
2023-03-11 17:10:54 -0400

    reorganizing infobox structure

--- src/Components/Infobox.js
+++ src/Components/Infobox.js
@@ -5,7 +5,7 @@ import styled from "styled-components";
 const myBackgroundColor = "white";
 
 const pastelColours = [
-    "#77dd77","#836953","#89cff0","#99c5c4","#9adedb","#aa9499","#aaf0d1","#b2fba5","#b39eb5","#bdb0d0","#bee7a5","#befd73","#c1c6fc","#c6a4a4","#c8fb0","#cb99c9","#cef0cc","#cfcfc4","#d6fffe","#d8a1c4","#dea5a4","#deece1","#dfd8e1","#e5d9d3","#e9d1bf","#f49ac2","#f4bfff","#fdfd96","#ff6961","#ff964f","#ff9899","#ffb7ce","#ca9bf7"];
+    "#77dd77","#836953","#89cff0","#99c5c4","#aa9499","#aaf0d1","#b2fba5","#9adedb","#b39eb5","#bdb0d0","#bee7a5","#befd73","#c1c6fc","#c6a4a4","#c8fb0","#cb99c9","#cef0cc","#cfcfc4","#d6fffe","#d8a1c4","#dea5a4","#deece1","#dfd8e1","#e5d9d3","#e9d1bf","#f49ac2","#f4bfff","#fdfd96","#ff6961","#ff964f","#ff9899","#ffb7ce","#ca9bf7"];
 
 
 //STYLED COMPONENTS
@@ -31,13 +31,23 @@ const InfoboxButton = styled.button`
   margin-bottom: 10px;
 `;
 
-const Wrapper = styled.section`
+const InfoboxWrapper = styled.section`
   background-color: ${props => props.bgColor ? props.bgColor : "white"};
   border: 2px solid black;
   width: 50%;
   margin: auto;
   margin-bottom: 5%;
   min-height: 20rem;
+
+  & .imageText {
+    position: relative;
+    top: -25px;
+    right: -3px;
+    color: white; 
+    height: 0px;
+    font-weight: 1000;
+    text-shadow: 0 0 1px black, 0 0 1px black, 0 0 1px black, 0 0 1px black;
+      }
 `;
 
 
@@ -45,14 +55,15 @@ const Wrapper = styled.section`
 function Infobox(props) {
   const boxData = props.data;
   return (
-    <Wrapper bgColor={pastelColours[boxData["id"]]}>
+    <InfoboxWrapper bgColor={pastelColours[boxData["id"]]}>
       <InfoboxImage src={"stockphotos/" + boxData["photo"]} />
+      <div className="imageText">{boxData["category"]}</div>
       <InfoboxTitle>{boxData["title"]}</InfoboxTitle>
       <InfoboxButtonArea>
           <InfoboxButton>Option 1</InfoboxButton>
           <InfoboxButton>Option 2</InfoboxButton>
       </InfoboxButtonArea>
-    </Wrapper>
+    </InfoboxWrapper>
   );
 }
 
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,37 +1,5 @@
 import Infobox from './Infobox'
-
-
-const questionData = {
-    0: {
-    id:0,
-    category: "Rumour",
-    title: "Will Elon Musk tweet about Dogecoin this week?",
-    photo: "elon.png",
-    options: ["Yes", "No"],
-    openDate: new Date("2022-01-15"),
-    closeDate: new Date("2023-01-15"),
-  },
-  1: {
-    id:1,
-    category: "Politics",
-    title: "Who will win Oscars tonight?",
-    photo: "oscar.png",
-    options: ["Dean Tsaltas", "Naruto Uzumaki", "Tim Horton", "Someone Else"],
-    openDate: new Date("2022-01-15"),
-    closeDate: new Date("2023-01-15"),
-  },
-  2: {
-    id:2,
-    category: "Gaming",
-    photo: "gamer.png",
-    title: "Will Counter Strike 2 announced this month?",
-    options: ["Yes", "No"],
-    openDate: new Date("2022-01-15"),
-    closeDate: new Date("2023-01-15"),
-  },
-}
-;
-
+import questionData from './FakeData';
 
 
 
@@ -40,9 +8,15 @@ function Main()
 {
     return(
             <div className="mainArea">
-            <Infobox data={questionData[0]}/>
-            <Infobox data={questionData[1]}/>
-            <Infobox data={questionData[2]}/>
+            <Infobox className="Infobox" data={questionData[0]}/>
+            <Infobox className="Infobox" data={questionData[1]}/>
+            <Infobox className="Infobox" data={questionData[2]}/>
+            <Infobox className="Infobox" data={questionData[3]}/>
+            <Infobox className="Infobox" data={questionData[4]}/>
+            <Infobox className="Infobox" data={questionData[5]}/>
+            <Infobox className="Infobox" data={questionData[6]}/>
+            <Infobox className="Infobox" data={questionData[7]}/>
+            <Infobox className="Infobox" data={questionData[8]}/>
             </div>
     )
 }
