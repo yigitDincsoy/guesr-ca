1b660b93f8cc2a57b813380e34f9a9870dc87f6e
Yigit Dincsoy <w0471994@campus.nscc.ca>
2023-03-13 15:15:50 -0300

    Added styled components to nav

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,20 +1,21 @@
 import {Link} from 'react-router-dom'
 import React, { useState } from 'react';
+import styled from "styled-components";
 
 function Navigation()
 {
     const [categorySelection, set_categorySelection] = useState("eyy");
     return(
         <div className="navbar">
-            <Link className="navlink" to="/">Popular</Link>
-            <Link className="navlink" to="/politics">Politics</Link>
-            <Link className="navlink" to="/economy">Economy</Link>
-            <Link className="navlink" to="/sports">Sports</Link>
-            <Link className="navlink" to="/news">News</Link>
-            <Link className="navlink" to="/gossip">Gossip</Link>
-            <Link className="navlink" to="/tech">Technology</Link>
-            <Link className="navlink" to="/all">All</Link>
-            <Link className="navlink" to="/market">Market</Link>
+            <Link className="navlink popular" to="/">Popular</Link>
+            <Link className="navlink politics" to="/politics">Politics</Link>
+            <Link className="navlink economy" to="/economy">Economy</Link>
+            <Link className="navlink sports" to="/sports">Sports</Link>
+            <Link className="navlink news" to="/news">News</Link>
+            <Link className="navlink gossip" to="/gossip">Gossip</Link>
+            <Link className="navlink tech" to="/tech">Technology</Link>
+            <Link className="navlink all" to="/all">All</Link>
+            <Link className="navlink market" to="/market">Market</Link>
         </div>
     )
 }
