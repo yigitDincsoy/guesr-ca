4a00bfd03814b0daee23778644af8027357311e6
Tulio Torezan <tuliotorezan@gmail.com>
2023-03-14 18:57:45 -0300

    model controler and router for item data

---
+++ server/models/item-model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Item = new Schema(
+    {
+        name: { type: String, required: true},
+        slot: { type: String, required: true},
+        cost: {type: Number, required: true},
+        sprite: {data: Buffer, contentType: String, required: true},
+        description: {type: String, required: true},
+    }
+)
+
+module.exports = mongoose.model('items', Item)
