4385567989a9d59e8147c8527196680b41690248
TYCardinals <116571749+TYCardinals@users.noreply.github.com>
2023-03-23 13:38:48 -0300

    CSS updates

--- src/Components/Auth/Login.js
+++ src/Components/Auth/Login.js
@@ -1,30 +1,43 @@
-import './login.css'
-import {useAuth} from './AuthProvider'
-import {useRef} from 'react'
+import "./login.css";
+import { useAuth } from "./AuthProvider";
+import { useRef } from "react";
 
-function Login()
-{
-    const authContext = useAuth();
-    const userRef = useRef();
-    const pwdRef = useRef();
+function Login() {
+  const authContext = useAuth();
+  const userRef = useRef();
+  const pwdRef = useRef();
 
-    function onAttemptLogin(e)
-    {
-        e.preventDefault();
-        authContext.signin(userRef.current.value,pwdRef.current.value)
-    }
+  function onAttemptLogin(e) {
+    e.preventDefault();
+    authContext.signin(userRef.current.value, pwdRef.current.value);
+  }
 
-    return(
-        <div className="loginform">
-            <form>
-            <h3>Login</h3>
-                <input type="text" placeholder="Email" required pattern=".*(.com|.net|.org|.ca)$"ref={userRef}></input><br/>
-                <input type="password" placeholder="Password" minlength="4" ref={pwdRef}></input><br/>
-                <button onClick={onAttemptLogin}>Login</button>
-            </form>
-        </div>
-    )
+  return (
+    <div class="container">
+      <div class="top"></div>
+      <div class="bottom"></div>
+      <div class="center"></div>
+      <form>
+        <h1>Login</h1>
+        <input
+          type="text"
+          placeholder="Email"
+          required
+          pattern=".*(.com|.net|.org|.ca)$"
+          ref={userRef}
+        ></input>
+        <br />
+        <input
+          type="password"
+          placeholder="Password"
+          minlength="4"
+          ref={pwdRef}
+        ></input>
+        <br />
+        <button onClick={onAttemptLogin}>Login</button>
+      </form>
+    </div>
+  );
 }
 
-
-export default Login
+export default Login;
