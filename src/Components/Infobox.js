92a7c6860d286c936c87ca698bfb4a329c5bf516
Tara Young <taralynyoung@gmail.com>
2023-03-24 11:22:22 -0300

    CSS updates

--- src/Components/Infobox.js
+++ src/Components/Infobox.js
@@ -7,15 +7,18 @@ import './Infobox.css';
 
 //Styled Components
 const InfoboxTitle = styled.h1`
-  font-size: 20px;
+  font-size: 18px;
+  font-family: poppins;
   text-align: center;
+  position: relative;
+  top: -10px;
   color: white;
-  min-height: 3rem;
-  max-height: 3rem;
+  /* min-height: 3rem; */
+  /* max-height: 3rem; */
   display: flex;
   align-items: center;
   justify-content: center;
-  line-height: 25px;
+  line-height: 20px;
   letter-spacing: .25px;
 `;
 
@@ -28,30 +31,31 @@ const InfoboxImage = styled.img`
 const InfoboxButtonArea = styled.div`
   display: flex;
   justify-content: space-evenly;
-  padding: 10px 0px 15px 0px;
+  padding: 5px 0px 10px 0px;
 `;
 
 const InfoboxButton = styled.button`
-  width: 44%;
+  width: 45%;
   padding: 12px;
   border-radius: 10px;
   border-style: none;
   margin-bottom: 8px;
-  font-family: roboto;
-  font-size: 16px;
+  font-family: poppins;
   font-weight: bold;
-  //box-shadow:  1px 1px 1px #d3d3d3;
+  font-size: 16px;
+  letter-spacing: -.5px;
+  /* font-weight: bold; */
+  box-shadow:  1.5px 1px 2.5px #63666A;
 `;
 
 const InfoboxWrapper = styled.section`
   // border: 5px solid white;
   width: 25%;
-  height: 40%;
   // margin: auto;
-  margin-bottom: 3%;
+  margin-bottom: 2%;
   margin-left: 1%;
   margin-right: 1%;
-  padding: 10px 10px 5px 10px;
+  padding: 15px 10px 5px 10px;
   // min-height: 30rem; 
   // max-height: 30rem;
   border-radius: 15px;
@@ -66,16 +70,16 @@ const InfoboxWrapper = styled.section`
     font-size: 28px;
     padding-left: 9px;
     position: relative;
-    top: -28px;
+    top: -35px;
     right: -3px;
     color: white;
     font-weight: bold;
-    letter-spacing: -1px;
+    letter-spacing: -1.1px;
     text-shadow: 0 0 1px black;
   }
 
   & .Green {
-    margin-left: 20px;
+    margin-left: 10px;
     background-color: green;
     opacity: 50%;
     padding: 5px;
@@ -104,13 +108,13 @@ function Infobox(props) {
     <InfoboxWrapper className={boxData["category"]}>
       <InfoboxImage src={"stockphotos/" + boxData["photo"]} />
       <div className="imageText">{boxData["category"]}</div>
-      <p>5 Days 3 Hours</p>
+      <p id="boxTimer">5 days 3 hours</p>
       <InfoboxTitle>{boxData["title"]}</InfoboxTitle>
       <InfoboxButtonArea>
         <InfoboxButton onClick={() => boxSelected()}>
           yes ({boxData["rewardRates"][0]})
         </InfoboxButton>
-        <InfoboxButton>no ({boxData["rewardRates"][1]})</InfoboxButton>
+        <InfoboxButton><i class="icon-thumbs-down"></i> ({boxData["rewardRates"][1]})</InfoboxButton>
       </InfoboxButtonArea>
       <p>
         <span className="Green">{boxData["totalGuesses"][0]} <i class="icon-thumbs-up"></i></span>
