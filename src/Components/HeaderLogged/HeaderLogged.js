1d6263a90858bd1226bebfa3ae709783c8e36a7e
Yigit <w0471994@campus.nscc.ca>
2023-04-12 12:20:48 -0300

    added loggedinheader

---
+++ src/Components/HeaderLogged/HeaderLogged.js
@@ -0,0 +1,63 @@
+import "./HeaderLogged.css";
+import React, { useState, useContext } from "react";
+import { GlobalContext } from "../../App";
+
+function HeaderLogged() {
+  const localGlobal = useContext(GlobalContext);
+
+  function bottomMenuActivation(arg1) {
+    if (localGlobal.bottomUIopen == false) {
+      document.getElementById("guessButton").style.borderWidth = "0 0 1px";
+      localGlobal.set_bottomUIopen(true);
+    } else {
+      localGlobal.set_bottomUIopen(false);
+      document.getElementById("guessButton").style.borderWidth = "0 0 4px";
+    }
+
+    console.log(localGlobal.bottomUIopen);
+  }
+
+  return (
+    <header>
+      <div className="header_left">
+      <button
+          className="buttonTop"
+          id="guessButton"
+          onClick={() => bottomMenuActivation()}
+        >LOGGED IN</button>
+        <button
+          className="buttonTop"
+          id="guessButton"
+          onClick={() => bottomMenuActivation()}
+        >WOAAAAH</button>
+      </div>
+      <div className="header_mid">
+        <h1>guesr</h1>
+        {/* <p>
+          we steal your time & data for our own profit, since 2023
+        </p> */}
+      </div>
+
+      <div className="header_right">
+        <button
+          className="buttonTop"
+          id="guessButton"
+          onClick={() => bottomMenuActivation()}
+        >
+          BRO YOU ARE LOGGED IN
+        </button>
+        <button className="buttonTop minibutton">🔔</button>
+        <button className="buttonTop minibutton">...</button>
+      </div>
+    </header>
+  );
+}
+
+export default HeaderLogged;
+
+//640 360
+
+// F3EEDF
+// CB884E
+// 8E858B
+// 945C57
