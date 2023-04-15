e79389a2772f10f0f8b7045abbffed7a1286b63d
Tulio Torezan <tuliotorezan@gmail.com>
2023-04-15 11:54:24 -0300

    Added votes attribute to question model

--- server/models/question-model.js
+++ server/models/question-model.js
@@ -6,6 +6,7 @@ const Question = new Schema(
         title: { type: String, required: true},
         category: { type: String, required: true},
         alternatives: {type: [String], required:true},
+        votes: {type: [Number], required:true},
         openDate: { type: Date, required: true},
         closeDate: { type: Date, required: true},
         cost: {type: Number, required: true},
